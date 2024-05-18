0019fd87b83b6bbf552fb70a0d0b99f3a5df0e3d
Bintang Anandhiya <bintanganandhiya23@gmail.com>
2024-05-18 22:55:33 +0700

    Initial Commit

---
+++ OfficeHuntAlpha/OfficeHuntAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeHuntAlphaApp.swift
+//  OfficeHuntAlpha
+//
+//  Created by Bintang Anandhiya on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct OfficeHuntAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
